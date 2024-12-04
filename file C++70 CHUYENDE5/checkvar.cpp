cbd7f4e175eec1b70218f457b423e7f7222e59ff
Adsovetzky <adsovetzky@gmail.com>
2024-12-04 22:38:54 +0700

    initial commit

---
+++ file C++70 CHUYENDE5/checkvar.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    freopen("checkvar.inp","r",stdin);
+    freopen("checkvar.out","w",stdout);
+    
+    cout<<"Hello world!";
+    
+    return 0;
+}
